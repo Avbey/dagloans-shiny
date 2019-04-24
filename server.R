3d8d665422694d400b93baab21e313da1722f6b0
avbey <azovsky777@gmail.com>
2019-04-24 16:32:05 +0300

    added sample lexical map

--- server.R
+++ server.R
@@ -2,10 +2,12 @@ source("pages/about.R")
 source("pages/how-to-cite.R")
 source("pages/the-database.R")
 source("pages/mapsurv.R")
+source("pages/maplex.R")
 
 shinyServer(function(input, output) {
   output$about <- renderUI({aboutPage})
   output$howto <- renderUI({howPage})
   output$theDatabase <- renderUI({databasePage})
   output$mapsurv <- renderUI({mapSurvPage})
+  output$maplex <- renderUI({mapLexPage})
 })
--- ui.R
+++ ui.R
@@ -39,7 +39,7 @@ body <- dashboardBody(
     ),
 
     tabItem(tabName = "maplex",
-            h2("upcoming")
+            uiOutput("maplex")
     ),
 
     tabItem(tabName = "sourceslex",
