84f1af3f1ef4d2427488f9b72a4aa515159bfc00
avbey <azovsky777@gmail.com>
2019-06-07 02:21:22 +0300

    modified data combination method for turkic influence mediation

--- pages/mediation-turkic.R
+++ pages/mediation-turkic.R
@@ -1,33 +1,30 @@
+sum_sibcls <- function(df, name_str) {
+  df1 <- data.frame(df[ , grepl( name_str , names( df ))])
+  return (rowSums(df1[sapply(df1, is.numeric)], na.rm = FALSE))
+}
+
+aggregate_df <- function(df, unique_names) {
+  result <- setNames(data.frame(matrix(ncol = length(unique_names), nrow = nrow(df))), c(unique_names))
+  for(village_name in unique_names) {
+    result[village_name] <- sum_sibcls(df, village_name)
+  }
+  return(as.data.frame(ifelse(result > 0, 1, 0)))
+}
+
 all_turkic_wide <- spread(all_turkic, Lexeme, Present)
-all_turkic_wide <- all_turkic_wide[all_turkic_wide$District %in% c("Rutul"),]
 row.names(all_turkic_wide) <- all_turkic_wide$Speaker
 all_turkic_wide <- all_turkic_wide[,-c(1:4)]
 all_turkic_wide <- t(all_turkic_wide)
-all_turkic_wide <- as.data.frame(all_turkic_wide)
-
 turkic=as.matrix(all_turkic_wide)
 turkic[is.na(turkic)] <- 0
-turkic=as.data.frame(turkic)
-all_turkic_wide$Mikik <- turkic$Mikik1 + turkic$Mikik2
-all_turkic_wide$Helmets <- turkic$Helmets1 + turkic$Helmets2 + turkic$Helmets3
-all_turkic_wide$Kina <- turkic$Kina1 + turkic$Kina2 + turkic$Kina3
-all_turkic_wide$Ikhrek <- turkic$Ikhrek1 + turkic$Ikhrek2 + turkic$Ikhrek3 + turkic$Ikhrek4
-all_turkic_wide$Rutul <- turkic$Rutul1
-all_turkic_wide$Kiche <- turkic$Kiche1 + turkic$Kiche2
-all_turkic_wide$Khlut <- turkic$Khlut1 + turkic$Khlut2 + turkic$Khlut3 + turkic$Khlut4 + turkic$Khlut5
-all_turkic_wide <- all_turkic_wide[,-c(1:21)]
-all_turkic_wide <- ifelse(all_turkic_wide > 0, 1, 0)
-all_turkic_wide <- as.data.frame(all_turkic_wide)
+all_turkic_wide=as.data.frame(turkic)
+unique_names <- c(unique(gsub('[[:digit:]]+', '', names(all_turkic_wide))))
+all_turkic_wide_mod <- aggregate_df(all_turkic_wide, unique_names)
 
-all_turkic_lgs <- all_turkic_wide
-all_turkic_lgs$Tsakhur <- all_turkic_wide$Mikik + all_turkic_wide$Helmets
-all_turkic_lgs$Rutul <- all_turkic_wide$Rutul + all_turkic_wide$Kina + all_turkic_wide$Ikhrek + all_turkic_wide$Kiche
-all_turkic_lgs$Lezgian <- all_turkic_wide$Khlut
-all_turkic_lgs <- all_turkic_lgs[,-c(1:4,6,7)]
-all_turkic_lgs <- ifelse(all_turkic_lgs > 0, 1, 0)
-all_turkic_lgs <- as.data.frame(all_turkic_lgs)
-
-ups2 <- renderPlot({
-  upset(all_turkic_lgs, 
-      order.by="degree", sets = colnames(all_turkic_lgs), matrix.color="blue", point.size=5)
-})
+# all_turkic_lgs <- all_turkic_wide
+# all_turkic_lgs$Tsakhur <- all_turkic_wide$Mikik + all_turkic_wide$Helmets
+# all_turkic_lgs$Rutul <- all_turkic_wide$Rutul + all_turkic_wide$Kina + all_turkic_wide$Ikhrek + all_turkic_wide$Kiche
+# all_turkic_lgs$Lezgian <- all_turkic_wide$Khlut
+# all_turkic_lgs <- all_turkic_lgs[,-c(1:4,6,7)]
+# all_turkic_lgs <- ifelse(all_turkic_lgs > 0, 1, 0)
+# all_turkic_lgs <- as.data.frame(all_turkic_lgs)
--- server.R
+++ server.R
@@ -137,15 +137,15 @@ shinyServer(function(input, output) {
   }
   
   startEnd <- reactive({
-    startEnd <- FindStartEnd(all_turkic_wide)
+    startEnd <- FindStartEnd(all_turkic_wide_mod)
   })
   
   output$sets <- renderUI({
-      data <- all_turkic_wide[startEnd()[1]:startEnd()[2]]
+      data <- all_turkic_wide_mod[startEnd()[1]:startEnd()[2]]
       top <- colSums(data)
       top <- as.character(head(names(top[order(top, decreasing = T)]), 7))
       sets <- selectInput('upset_sets', label="Select villages (at least two) ",
-                          choices = as.character(colnames(all_turkic_wide[ , startEnd()[1]:startEnd()[2]])),
+                          choices = as.character(colnames(all_turkic_wide_mod[ , startEnd()[1]:startEnd()[2]])),
                           multiple=TRUE, selectize=TRUE, selected = top)
     return(sets)
   })
@@ -162,7 +162,7 @@ shinyServer(function(input, output) {
   output$mediationSpeakers <- renderUI({mediationSpeakersPage})
   output$mediationVillages <- renderUI({mediationVillagesPage})
   output$mediationTurkic <- renderPlot({
-    upset(all_turkic_wide, 
+    upset(all_turkic_wide_mod, 
           sets = Specific_sets(), 
           nintersects = input$nintersections,
           point.size = input$pointsize,
@@ -203,11 +203,11 @@ shinyServer(function(input, output) {
       else
         pdf(file, width = width/100, height = height/100, onefile=FALSE)
       
-      upset(all_turkic_wide,
+      upset(all_turkic_wide_mod,
             nintersects = input$nintersections,
             point.size = input$pointsize,
             line.size = line_size(),
-            sets = colnames(all_turkic_wide),
+            sets = colnames(all_turkic_wide_mod),
             order.by = orderdat(),
             main.bar.color= main_bar_color(),
             sets.bar.color= sets_bar_color(),
